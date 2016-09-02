08223799e5285f732d53474a37374c81126edd5d
KitsenkoDmitry <kitsenkodv@gmail.com>
2016-09-02 10:55:54 +0300

    Доработка по критериям, минимизация css и js

--- js/script.js
+++ js/script.js
@@ -3,7 +3,9 @@ var popup = document.querySelector(".search-form");
 var popup_show = false;
 
 form_btn.addEventListener("click", function(event) {
+  event.preventDefault();
   if(!popup_show) {
+    console.log("hello");
     popup.classList.add("model-content-show");
     popup.classList.remove("model-content-close");
     popup_show = true;
